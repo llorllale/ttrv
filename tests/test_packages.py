7cb9fb0046e6d2186a4601bb24473d920804f7dc
deepend-tildeclub <deepend@tilde.club>
2019-12-11 15:51:00 -0700

    removed tests folder

--- tests/test_packages.py
+++
@@ -1,8 +0,0 @@
-from tvr import packages
-
-
-def test_praw3_package():
-    # Sanity check that the package was installed
-    assert packages.praw
-    assert len(packages.__praw_hash__) == 40
-    assert packages.__praw_bundled__ is True
